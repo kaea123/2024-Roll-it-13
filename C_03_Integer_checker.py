96d7db24157420071b00fdaef8eb3e36893109f1
kaea123 <159498027+kaea123@users.noreply.github.com>
2024-02-15 22:56:17 +1300

    Updated Roll It 13, fixed integer checker

--- C_03_Integer_checker.py
+++ C_03_Integer_checker.py
@@ -1,20 +1,25 @@
 
 # Checks that users enter an integer
 # that is more than 13
-while True:
+def int_check():
 
-    error = "Please enter an integer that is 13 or  more. "
+    while True:
+        error = "Please enter an integer that is 13 or  more. "
 
-    try:
-        my_num = int(input("Enter an integer: "))
+        try:
+            response = int(input("Enter an integer: "))
 
-        # checks that the number is more than / equal to 13
+            # checks that the number is more than / equal to 13
+            if response < 13:
+                print(error)
+            else:
+                return response
 
-        if my_num < 13:
+        except ValueError:
             print(error)
-        else:
-            print("Your number is ", my_num)
 
-    except ValueError:
-        print(error)
+
+# main routine goes here
+target_score = int_check()
+print(target_score)
 
